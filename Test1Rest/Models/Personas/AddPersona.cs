66008bb0c077a8ee06b33d235894e095bdc96343
Santiago Guzman <guzman.santiago135@gmail.com>
2022-07-01 01:13:46 -0300

    .

---
+++ Test1Rest/Models/Personas/AddPersona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test1Rest.Models.Personas
+{
+    public class AddPersona : Conexion
+    {
+        protected override void Process()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Select()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
