990e35b7ecb65bd9634ded70aba0044c87f99ef5
Santiago Guzman <guzman.santiago135@gmail.com>
2022-07-01 01:17:04 -0300

    .

--- Test1Rest/Models/Personas/AddPersona.cs
+++ Test1Rest/Models/Personas/AddPersona.cs
@@ -7,6 +7,7 @@ namespace Test1Rest.Models.Personas
 {
     public class AddPersona : Conexion
     {
+        Persona persona;
         protected override void Process()
         {
             throw new NotImplementedException();
